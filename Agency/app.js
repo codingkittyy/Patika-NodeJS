f9406f2fe42e1f4013d04bbfb75be7a73a26e02b
tecosama <taylancankose@gmail.com>
2022-06-29 23:05:31 +0300

    edited

--- Agency/app.js
+++ Agency/app.js
@@ -32,7 +32,7 @@ mongoose
 app.use('/', pageRoute)
 app.use('/portfolio', photoRoute)
 
-const port = 3000
+const port = process.env.PORT || 5000
 
 app.listen(port, () =>
   console.log(`Agency App Project is running on port: ${port}`)
